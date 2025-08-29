6e605c07a88be7b288728ac25991eb8b47aaf715
biancviana <bvitoriaviana@gmail.com>
2025-08-29 00:08:10 -0300

    Melhoro xlm e classe main

--- projeto-calculadora-mobile/app/src/main/java/com/example/calculadora/MainActivity.java
+++ projeto-calculadora-mobile/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
